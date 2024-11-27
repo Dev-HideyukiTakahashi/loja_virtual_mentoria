c9a4cc88026dfe446322ba1f85acb0e59e86e6c5
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-11-27 06:12:01 -0300

    fix: pessoa
    Foi usado o @Inheritance e nesse caso
    o GenerationType.IDENTITY não funciona sendo necessário ser 'AUTO'
    para as child

--- src/main/java/com/jdev/mentoria/lojavirtual/model/Pessoa.java
+++ src/main/java/com/jdev/mentoria/lojavirtual/model/Pessoa.java
@@ -1,20 +1,34 @@
 package com.jdev.mentoria.lojavirtual.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.OneToMany;
 
-@MappedSuperclass
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Pessoa {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
   private String nome;
   private String email;
   private String telefone;
+
+  /* orphanRemoval: remove endereço ao deletar */
+  /* fetch = FetchType.LAZY , só carrega endereço no GET */
+  @OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+  private List<Endereco> enderecos = new ArrayList<>();
 
   public Pessoa() {
   }
