8c9625593ad6cafa6d4c224f957970b4db8acef7
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-11-28 01:42:35 -0300

    criado nota fiscal model

---
+++ src/main/java/com/jdev/mentoria/lojavirtual/model/NotaFiscalCompra.java
@@ -0,0 +1,163 @@
+package com.jdev.mentoria.lojavirtual.model;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+public class NotaFiscalCompra {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String numeroNota;
+  private String serieNota;
+  private String descricaoObs;
+  private BigDecimal valorTotal;
+  private BigDecimal valorDesconto;
+  private BigDecimal valorIcms;
+
+  @Temporal(TemporalType.DATE)
+  private Date dataCompra;
+
+  @ManyToOne
+  @JoinColumn(foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+  private Pessoa pessoa;
+
+  @ManyToOne
+  @JoinColumn(foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "conta_pagar_fk"))
+  private ContaPagar contaPagar;
+
+  public NotaFiscalCompra() {
+  }
+
+  public NotaFiscalCompra(String numeroNota, String serieNota, String descricaoObs, BigDecimal valorTotal,
+      BigDecimal valorDesconto, BigDecimal valorIcms, Date dataCompra, Pessoa pessoa, ContaPagar contaPagar) {
+    this.numeroNota = numeroNota;
+    this.serieNota = serieNota;
+    this.descricaoObs = descricaoObs;
+    this.valorTotal = valorTotal;
+    this.valorDesconto = valorDesconto;
+    this.valorIcms = valorIcms;
+    this.dataCompra = dataCompra;
+    this.pessoa = pessoa;
+    this.contaPagar = contaPagar;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNumeroNota() {
+    return numeroNota;
+  }
+
+  public void setNumeroNota(String numeroNota) {
+    this.numeroNota = numeroNota;
+  }
+
+  public String getSerieNota() {
+    return serieNota;
+  }
+
+  public void setSerieNota(String serieNota) {
+    this.serieNota = serieNota;
+  }
+
+  public String getDescricaoObs() {
+    return descricaoObs;
+  }
+
+  public void setDescricaoObs(String descricaoObs) {
+    this.descricaoObs = descricaoObs;
+  }
+
+  public BigDecimal getValorTotal() {
+    return valorTotal;
+  }
+
+  public void setValorTotal(BigDecimal valorTotal) {
+    this.valorTotal = valorTotal;
+  }
+
+  public BigDecimal getValorDesconto() {
+    return valorDesconto;
+  }
+
+  public void setValorDesconto(BigDecimal valorDesconto) {
+    this.valorDesconto = valorDesconto;
+  }
+
+  public BigDecimal getValorIcms() {
+    return valorIcms;
+  }
+
+  public void setValorIcms(BigDecimal valorIcms) {
+    this.valorIcms = valorIcms;
+  }
+
+  public Date getDataCompra() {
+    return dataCompra;
+  }
+
+  public void setDataCompra(Date dataCompra) {
+    this.dataCompra = dataCompra;
+  }
+
+  public Pessoa getPessoa() {
+    return pessoa;
+  }
+
+  public void setPessoa(Pessoa pessoa) {
+    this.pessoa = pessoa;
+  }
+
+  public ContaPagar getContaPagar() {
+    return contaPagar;
+  }
+
+  public void setContaPagar(ContaPagar contaPagar) {
+    this.contaPagar = contaPagar;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    NotaFiscalCompra other = (NotaFiscalCompra) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+}
---
+++ src/main/java/com/jdev/mentoria/lojavirtual/model/NotaItemProduto.java
@@ -0,0 +1,95 @@
+package com.jdev.mentoria.lojavirtual.model;
+
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class NotaItemProduto {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private Double quantidade;
+
+  @ManyToOne
+  @JoinColumn(foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "nota_fisca_compra_fk"))
+  private NotaFiscalCompra notaFiscalCompra;
+
+  @ManyToOne
+  @JoinColumn(foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
+  private Produto produto;
+
+  public NotaItemProduto() {
+  }
+
+  public NotaItemProduto(Double quantidade, NotaFiscalCompra notaFiscalCompra, Produto produto) {
+    this.quantidade = quantidade;
+    this.notaFiscalCompra = notaFiscalCompra;
+    this.produto = produto;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Double getQuantidade() {
+    return quantidade;
+  }
+
+  public void setQuantidade(Double quantidade) {
+    this.quantidade = quantidade;
+  }
+
+  public NotaFiscalCompra getNotaFiscalCompra() {
+    return notaFiscalCompra;
+  }
+
+  public void setNotaFiscalCompra(NotaFiscalCompra notaFiscalCompra) {
+    this.notaFiscalCompra = notaFiscalCompra;
+  }
+
+  public Produto getProduto() {
+    return produto;
+  }
+
+  public void setProduto(Produto produto) {
+    this.produto = produto;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    NotaItemProduto other = (NotaItemProduto) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+}
