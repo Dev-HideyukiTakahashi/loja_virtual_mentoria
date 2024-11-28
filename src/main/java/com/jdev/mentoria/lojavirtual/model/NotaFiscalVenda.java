1a9f41e565aa62aea8b79380cd23f26a1ecb222c
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-11-28 02:47:11 -0300

    criado nota fiscal venda

---
+++ src/main/java/com/jdev/mentoria/lojavirtual/model/NotaFiscalVenda.java
@@ -0,0 +1,110 @@
+package com.jdev.mentoria.lojavirtual.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class NotaFiscalVenda {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String numero;
+  private String serie;
+  private String tipo;
+
+  @Column(columnDefinition = "TEXT")
+  private String xml;
+
+  @Column(columnDefinition = "TEXT")
+  private String pdf;
+
+  public NotaFiscalVenda() {
+  }
+
+  public NotaFiscalVenda(String numero, String serie, String tipo, String xml, String pdf) {
+    this.numero = numero;
+    this.serie = serie;
+    this.tipo = tipo;
+    this.xml = xml;
+    this.pdf = pdf;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNumero() {
+    return numero;
+  }
+
+  public void setNumero(String numero) {
+    this.numero = numero;
+  }
+
+  public String getSerie() {
+    return serie;
+  }
+
+  public void setSerie(String serie) {
+    this.serie = serie;
+  }
+
+  public String getTipo() {
+    return tipo;
+  }
+
+  public void setTipo(String tipo) {
+    this.tipo = tipo;
+  }
+
+  public String getXml() {
+    return xml;
+  }
+
+  public void setXml(String xml) {
+    this.xml = xml;
+  }
+
+  public String getPdf() {
+    return pdf;
+  }
+
+  public void setPdf(String pdf) {
+    this.pdf = pdf;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    NotaFiscalVenda other = (NotaFiscalVenda) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+}
