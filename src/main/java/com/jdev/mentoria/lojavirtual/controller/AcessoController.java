d077b2060833c9375d8d7a003d732a47f4fe4ea3
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-12-01 05:55:15 -0300

    acesso - novo cadastro

--- src/main/java/com/jdev/mentoria/lojavirtual/controller/AcessoController.java
+++ src/main/java/com/jdev/mentoria/lojavirtual/controller/AcessoController.java
@@ -1,22 +1,27 @@
 package com.jdev.mentoria.lojavirtual.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jdev.mentoria.lojavirtual.model.Acesso;
 import com.jdev.mentoria.lojavirtual.service.AcessoService;
 
 @RestController
+@RequestMapping("/acessos")
 public class AcessoController {
 
   @Autowired
   private AcessoService acessoService;
 
   @PostMapping
-  public Acesso salvar(@RequestBody Acesso acesso) {
-    return acessoService.save(acesso);
+  public ResponseEntity<Acesso> salvar(@RequestBody Acesso obj) {
+    Acesso acesso = acessoService.save(obj);
+    return ResponseEntity.status(HttpStatus.CREATED).body(acesso);
   }
 
 }
